df33c171620c2002a1867de4836770389f5754d4
Derrick Lai <8101548+derricklai@users.noreply.github.com>
2025-10-17 10:26:38 +1100

    test: home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-blue-500 text-white">
+      <h1 className="text-4xl font-extrabold">Tailwind test 🔥</h1>
+    </div>
+  );
+}
+
+// intial test to see if tailwind is loading properly
